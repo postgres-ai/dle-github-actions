8b69d524c7bde521f8f5cfe377ddba9da62360c4
akartasov <agneum@gmail.com>
2021-04-07 18:27:56 +0700

    extends action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/sh -l
 
-echo "Owner $1"
+JSON_DATA=$(jq -n -c \
+  --arg owner "$INPUT_OWNER" \
+  --arg repo "$INPUT_REPO" \
+  --arg ref "$INPUT_REF" \
+  --arg commands "$INPUT_COMMANDS" \
+  '{repo: {owner: $owner, repo: $repo, ref: $ref}, commands: $commands | rtrimstr("\n") | split("\n")}')
+
+echo $JSON_DATA
+
+curl --location --request POST "${CI_ENDPOINT}" \
+--header "Authorization-Token: ${SECRET_TOKEN}" \
+--header 'Content-Type: application/json' \
+--data "${JSON_DATA}"
 
 status="OK"
 echo "::set-output name=status::$status"
