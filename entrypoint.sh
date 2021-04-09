7cdf046fa5866791970277ae7633dc345383e2f4
akartasov <agneum@gmail.com>
2021-04-09 21:16:22 +0700

    add new request parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,15 @@ JSON_DATA=$(jq -n -c \
   --arg repo "$INPUT_REPO" \
   --arg ref "$INPUT_REF" \
   --arg commands "$INPUT_COMMANDS" \
-  '{source: {owner: $owner, repo: $repo, ref: $ref}, commands: $commands | rtrimstr("\n") | split("\n")}')
+  --arg db_name "$INPUT_DBNAME" \
+  --arg actor "$GITHUB_ACTOR" \
+  --arg migration_envs "$INPUT_MIGRATION_ENVS" \
+  '{source: {owner: $owner, repo: $repo, ref: $ref}, actor: $actor, db_name: $db_name, commands: $commands | rtrimstr("\n") | split("\n"), migration_envs: $migration_envs | rtrimstr("\n") | split("\n")}')
 
 echo $JSON_DATA
 
 response=$(curl -s --location --request POST "${CI_ENDPOINT}" \
---header "Authorization-Token: ${SECRET_TOKEN}" \
+--header "Verification-Token: ${SECRET_TOKEN}" \
 --header 'Content-Type: application/json' \
 --data "${JSON_DATA}")
 
