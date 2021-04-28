3d50fc58fd287ad35194b7c035ac52a925513d18
akartasov <agneum@gmail.com>
2021-04-28 21:42:11 +0700

    feat: enrich migration info

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,12 @@ JSON_DATA=$(jq -n -c \
   --arg commands "$INPUT_COMMANDS" \
   --arg db_name "$INPUT_DBNAME" \
   --arg actor "$GITHUB_ACTOR" \
+  --arg branch "${GITHUB_HEAD_REF:-${GITHUB_REF##*/}}" \
+  --arg commit_sha "${INPUT_COMMIT_SHA}" \
+  --arg commit "${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/commit/${INPUT_COMMIT_SHA}" \
+  --arg request_link "${INPUT_PULL_REQUEST:-$INPUT_COMPARE}" \
   --arg migration_envs "$INPUT_MIGRATION_ENVS" \
-  '{source: {owner: $owner, repo: $repo, ref: $ref}, actor: $actor, db_name: $db_name, commands: $commands | rtrimstr("\n") | split("\n"), migration_envs: $migration_envs | rtrimstr("\n") | split("\n")}')
+  '{source: {owner: $owner, repo: $repo, ref: $ref, branch: $branch, commit_sha: $commit_sha, commit: $commit, request_link: $request_link}, actor: $actor, db_name: $db_name, commands: $commands | rtrimstr("\n") | split("\n"), migration_envs: $migration_envs | rtrimstr("\n") | split("\n")}')
 
 echo $JSON_DATA
 
